7172c06595c3831983d1b3d4265bb453a6fdba57
전형욱 <jim9823@naver.com>
2024-03-12 16:13:26 +0900

    ✨Feat :  added login page(+social login image files)

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+export default function Login(){
+    const messages = ["픽잇업이 제공하는 서비스를", "하나의 계정으로 모두 이용할 수 있습니다!"];
+    return (
+        <div className="grid place-items-center w-full h-[600px]">
+            <div className="absolute top-1/4 w-[25vw] h-[50vh]">
+                <div className="grid place-items-center">
+                    {messages.map((line, index) => (
+                        <div className="my-1" key={index}>
+                            {line}
+                        </div>
+                    ))}
+                </div>
+                <div className="w-full h-[5vh]"></div>
+                <div className="w-full h-[5vh]"></div>
+                <div className="w-full h-[5vh] py-5 my-4 flex items-center justify-start rounded-[10px] bg-[#FFEC00] whitespace-pre">
+                    <Image src="/images/kakaoLogo.png" width={40} height={40} alt="kakaoLogo" className="ml-[65px]" />  카카오 계정으로 로그인</div>
+                <div className="w-full h-[5vh] py-5 my-4 flex items-center justify-start rounded-[10px] text-white bg-[#03C75A] whitespace-pre">
+                    <Image src="/images/naverLogo.png" width={40} height={40} alt="naverLogo" className="ml-[65px]" />  네이버 계정으로 로그인</div>
+                <div className="w-full h-[5vh] py-5 my-4 flex items-center justify-start rounded-[10px] border border-[#f5f5f5] whitespace-pre">
+                    <Image src="/images/googleLogo.png" width={21} height={21} alt="googleLogo" className="ml-[74px]" />    구글 계정으로 로그인</div>
+                <div className="w-full h-[5vh] py-5 my-4 flex items-center justify-start rounded-[10px] border border-[#f5f5f5] whitespace-pre">
+                    <Image src="/images/pickItupLogo.png" width={24} height={21.84} alt="pickITupLogo" className="ml-[73px]" />    pick IT up 로그인</div>
+            </div>
+        </div>
+    )
+}
