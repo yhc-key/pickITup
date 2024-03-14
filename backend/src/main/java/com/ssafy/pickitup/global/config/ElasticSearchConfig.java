f978de3283e8a314c31fb3aec8c1420922a750d8
hyunsoo10 <hyunsoo4885@gmail.com>
2024-03-14 13:46:48 +0900

    👷Build :  write spring docker-compose file

--- backend/src/main/java/com/ssafy/pickitup/global/config/ElasticSearchConfig.java
+++ backend/src/main/java/com/ssafy/pickitup/global/config/ElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.pickitup.global.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -11,10 +12,14 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = "com.ssafy.pickitup.domain.recruit")
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
+    @Value("${spring.data.elasticsearch.cluster-nodes}")
+    private String ELASTICSEARCH_URL;
+
+
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-            .connectedTo("localhost:9200")
+            .connectedTo(ELASTICSEARCH_URL)
             .build();
         return RestClients.create(clientConfiguration).rest();
     }
