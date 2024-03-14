7082161f5f9d13d9bf96565f47db8ac1d1aa6839
조용환 <yhcho0712@gmail.com>
2024-03-14 17:37:48 +0900

    🐛Fix :  Fix error of editing myEssag ing...

--- frontend/src/app/myPage/info/myEssay/page.tsx
+++ frontend/src/app/myPage/info/myEssay/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import { useRef, useState } from "react";
 import { FaPlus, FaPen, FaChevronDown } from "react-icons/fa";
 
 export default function MyEssay() : JSX.Element {
-  const [isEditing, setIsEditing] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
+  const makeCanEditHandler = () => {
+  }
+
 
 
   return (<div className="w-full relative pt-3 pr-3">
@@ -14,9 +17,9 @@ export default function MyEssay() : JSX.Element {
   <div>과거 자소서 추가</div>
   </button>
   <div className="w-full flex flex-row border border-black rounded-lg mt-12 p-2 min-h-10 justify-between">
-    <input ref={inputRef} disabled={isEditing} placeholder="1. 당신이 입사한 이유가 무엇입니까?" className="w-full mr-10"/>
+    <input ref={inputRef} placeholder="1. 당신이 입사한 이유가 무엇입니까?" className="w-full mr-10"/>
     <div className="flex flex-row gap-6 mr-4 text-lg">
-      <button>
+      <button onClick={makeCanEditHandler}>
         <FaPen />
       </button>
       <button>
