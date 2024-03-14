148b204b3211a59e245da8d6265e88673ad6a81c
hyunsoo10 <hyunsoo4885@gmail.com>
2024-03-14 15:12:52 +0900

    🚑Hotfix :  fix CORS error

--- backend/spring-api/src/main/java/com/ssafy/pickitup/domain/auth/api/AuthController.java
+++ backend/spring-api/src/main/java/com/ssafy/pickitup/domain/auth/api/AuthController.java
@@ -21,6 +21,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"https://pickitup.online", "http://localhost:3000"}, exposedHeaders = "*")
 @RequestMapping("/auth")
 @Tag(name = "AuthController", description = "회원 인증 정보 관련 API")
 public class AuthController {
