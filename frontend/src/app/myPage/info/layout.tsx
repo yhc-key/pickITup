6c78d529c3ed10bcc7cb21058f3e4a14e699bd1a
전형욱 <jim9823@naver.com>
2024-03-18 18:00:01 +0900

    ✨Feat :  add mybadge page ing...

--- frontend/src/app/myPage/info/layout.tsx
+++ frontend/src/app/myPage/info/layout.tsx
@@ -12,22 +12,22 @@ export default function InfoLayout({
   const pathname = usePathname();
   const isActive = (path: string) => path === pathname;
   return <Fragment>
-  <div className="border-b-2 border-black flex flex-row items-center mr-4">
-    <Link href="/myPage/info/myFavoriteRecruit" className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center ${isActive("/myPage/info/myFavoriteRecruit") ? "border-t-4 border-f5green-300" : ""} `}>
+  <div className="border-b-2 border-black flex flex-row items-center">
+    <Link href="/myPage/info/myFavoriteRecruit" className={`w-1/4 text-f5 black-300 h-16 flex items-center justify-center ${isActive("/myPage/info/myFavoriteRecruit") ? "border-t-4 border-f5green-300" : ""} `}>
       <span>내가 찜한 채용공고</span>
     </Link>
-    <Link href="/myPage/info/myBadge" className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center ${isActive("/myPage/info/myBadge") ? "border-t-4 border-f5green-300" : ""} `}>
+    <Link href="/myPage/info/myBadge" className={`w-1/4 text-f5 black-300 h-16 flex items-center justify-center ${isActive("/myPage/info/myBadge") ? "border-t-4 border-f5green-300" : ""} `}>
       <span>나의 뱃지</span>
     </Link>
-    <Link href="/myPage/info/myPastAns" className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center ${isActive("/myPage/info/myPastAns") ? "border-t-4 border-f5green-300" : ""} `}>
+    <Link href="/myPage/info/myPastAns" className={`w-1/4 text-f5 black-300 h-16 flex items-center justify-center ${isActive("/myPage/info/myPastAns") ? "border-t-4 border-f5green-300" : ""} `}>
       <span>나의 과거 문제 내역</span>
     </Link>
-    <Link href="/myPage/info/myEssay" className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center ${isActive("/myPage/info/myEssay") ? "border-t-4 border-f5green-300" : ""} `}>
+    <Link href="/myPage/info/myEssay" className={`w-1/4 text-f5 black-300 h-16 flex items-center justify-center ${isActive("/myPage/info/myEssay") ? "border-t-4 border-f5green-300" : ""} `}>
       <span>자기소개서 관리</span>
     </Link>
 
   </div>
-  <div className="flex-grow ml-5">{children}</div>
+  <div className="flex-grow">{children}</div>
   </Fragment>
 
 }
