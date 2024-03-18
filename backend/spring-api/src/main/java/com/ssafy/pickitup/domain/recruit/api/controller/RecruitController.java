4a42bd1a45b20093a1de70f2f76695899a5c0806
송윤재 <1218song@naver.com>
2024-03-18 15:44:08 +0900

    💡Comment :  add swagger summary

--- backend/spring-api/src/main/java/com/ssafy/pickitup/domain/recruit/api/controller/RecruitController.java
+++ backend/spring-api/src/main/java/com/ssafy/pickitup/domain/recruit/api/controller/RecruitController.java
@@ -4,6 +4,8 @@ import com.ssafy.pickitup.domain.recruit.command.RecruitCommandElasticsearchRepo
 import com.ssafy.pickitup.domain.recruit.query.RecruitQueryService;
 import com.ssafy.pickitup.domain.recruit.query.dto.RecruitQueryRequestDto;
 import com.ssafy.pickitup.domain.recruit.query.dto.RecruitQueryResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,23 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/recruit")
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "RecruitController", description = "채용공고 관련 API")
 public class RecruitController {
 
     private final RecruitQueryService recruitQueryService;
     @Autowired
     private RecruitCommandElasticsearchRepository recruitCommandElasticsearchRepository;
 
+    @Operation(summary = "채용 공고 조회(전체)")
     @GetMapping("/{pageNo}")
     public Page<RecruitQueryResponseDto> getAllDocuments(@PathVariable Integer pageNo) {
 
         return recruitQueryService.searchAll(pageNo);
     }
 
+    @Operation(summary = "채용 공고 조회(키워드, 검색어)")
     @PostMapping("/search")
     public Page<RecruitQueryResponseDto> getDocuments(@RequestBody RecruitQueryRequestDto dto) {
         return recruitQueryService.search(dto);
     }
 
+    @Operation(summary = "Elasticsearch 데이터를 mongodb로 마이그레이션")
     @GetMapping("/read")
     public void read() {
         recruitQueryService.readKeywords();
--- backend/spring-api/src/main/java/com/ssafy/pickitup/domain/selfdocument/api/controller/SelfDocumentController.java
+++ backend/spring-api/src/main/java/com/ssafy/pickitup/domain/selfdocument/api/controller/SelfDocumentController.java
@@ -6,6 +6,8 @@ import com.ssafy.pickitup.domain.selfdocument.command.dto.MainQuestionCommandReq
 import com.ssafy.pickitup.domain.selfdocument.command.dto.SubQuestionCommandRequestDto;
 import com.ssafy.pickitup.domain.selfdocument.query.MainQuestionQueryService;
 import com.ssafy.pickitup.domain.selfdocument.query.SubQuestionQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/self")
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "SelfDocumentController", description = "자기소개서 관련 API")
 public class SelfDocumentController {
 
     private final MainQuestionQueryService mainQueryService;
@@ -28,11 +31,13 @@ public class SelfDocumentController {
     private final SubQuestionQueryService subQueryService;
     private final SubQuestionCommandService subCommandService;
 
+    @Operation(summary = "메인 질문 조회")
     @GetMapping("/main")
     public ResponseEntity<?> searchMain(@RequestParam("userId") Integer userId) {
         return ResponseEntity.ok(mainQueryService.searchMainQuestions(userId));
     }
 
+    @Operation(summary = "메인 질문 등록")
     @PostMapping("/main")
     public ResponseEntity<?> registerMain(
         @RequestBody MainQuestionCommandRequestDto dto, @RequestParam("userId") Integer userId) {
@@ -40,35 +45,44 @@ public class SelfDocumentController {
             mainCommandService.registerMainQuestion(dto, userId));
     }
 
+
+    @Operation(summary = "메인 질문 삭제")
     @DeleteMapping("/main/{mainId}")
     public ResponseEntity<?> deleteMain(@PathVariable Integer mainId) {
         return mainCommandService.deleteMainQuestion(mainId)
             ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
     }
 
+
+    @Operation(summary = "메인 질문 수정")
     @PatchMapping("/main/{mainId}")
     public ResponseEntity<?> patchMain(@PathVariable Integer mainId,
         @RequestBody MainQuestionCommandRequestDto dto) {
         return ResponseEntity.ok(mainCommandService.modifyMainQuestion(mainId, dto));
     }
 
+
+    @Operation(summary = "서브 질문 조회")
     @GetMapping("/main/{mainId}/sub")
     public ResponseEntity<?> searchSub(@PathVariable Integer mainId) {
         return ResponseEntity.ok(subQueryService.searchSubQuestions(mainId));
     }
 
+    @Operation(summary = "서브 질문 등록")
     @PostMapping("/main/{mainId}/sub")
     public ResponseEntity<?> registerSub(@PathVariable Integer mainId,
         @RequestBody SubQuestionCommandRequestDto dto) {
         return ResponseEntity.ok(subCommandService.registerSubQuestion(mainId, dto));
     }
 
+    @Operation(summary = "서브 질문 삭제")
     @DeleteMapping("/main/{mainId}/sub/{subId}")
     public ResponseEntity<?> deleteSub(@PathVariable Integer subId) {
         return subCommandService.deleteSubQuestion(subId)
             ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
     }
 
+    @Operation(summary = "서브 질문 수정")
     @PatchMapping("/main/{mainId}/sub/{subId}")
     public ResponseEntity<?> patchSub(@PathVariable Integer subId,
         @RequestBody SubQuestionCommandRequestDto dto) {
