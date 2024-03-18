4012dd1fc0b4045cb0b5965f76f835aa8f1b4763
hyeounguk2 <jim9823@naver.com>
2024-03-18 22:16:31 +0900

    📝Docs :  mypage - mybadge page finished, little change in login page(font size), added locked.png

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -9,17 +9,17 @@ export default function Login(){
           
           <form action="#" method="POST">
             <div className='flex w-full h-[6vh] justify-center items-center mt-14'>
-              <label htmlFor="id" className="w-[6vw] text-sm font-black">아이디</label>
+              <label htmlFor="id" className="w-[6vw] font-black">아이디</label>
               <input placeholder="아이디를 입력하세요" className="w-[20vw] ml-6 rounded-md bg-[#f5f5f5] border border-[#d9d9d9]"/>
             </div>
 
             <div className='flex w-full h-[6vh] justify-center items-center'>
-              <label htmlFor="password" className='w-[6vw] text-sm font-black'>비밀번호</label>
+              <label htmlFor="password" className='w-[6vw] font-black'>비밀번호</label>
               <input placeholder="비밀번호를 입력하세요" type="password" className="w-[20vw] ml-6 rounded-md bg-[#f5f5f5] border border-[#d9d9d9]"/>
             </div>
 
             <div className="flex w-full h-[14vh] justify-center items-center">
-              <button type="submit" className="w-[18vw] h-[5vh] rounded-md bg-[#00ce7c] text-white font-bold">로그인</button>
+              <button type="submit" className="w-[18vw] h-[5vh] rounded-md bg-[#00ce7c] text-white text-lg font-bold">로그인</button>
             </div>
           </form>
 
--- frontend/src/app/myPage/info/myBadge/page.tsx
+++ frontend/src/app/myPage/info/myBadge/page.tsx
@@ -1,62 +1,83 @@
+"use client"
 import { useEffect, useState, ReactElement } from "react";
 import Image from 'next/image';
-
 function MyBadge() {
-  let acquired:string[] = ["attend1","jobRead1","jobRead50","jobScrap1"];
+  let acquired:string[] = ["attend1","jobRead1","jobRead50","jobScrap1"]; //얻은 배지 정보
   let unacquired:string [] = ["attend50","attend100","blogRead1","blogRead50","blogRead100",
-                              "blogScrap1","blogScrap50","blogScrap100","blogWrite1","blogWrite50","blogWrite100",
-                              "game10","game30","game50","game150","game300","jobRead100","jobScrap50","jobScrap100"];
-  const map = new Map<String,String>();
-  map.set('attend1','출석1일');
-  map.set('attend50','출석50일');
-  map.set('attend100','출석100일');
-  map.set('blogRead1','블로그열람 1회');
-  map.set('blogRead50','블로그열람 50회');
-  map.set('blogRead100','블로그열람 100회');
-  map.set('blogScrap1','블로그스크랩 1회');
-  map.set('blogScrap50','블로그스크랩 50회');
-  map.set('blogScrap100','블로그스크랩 100회');
-  map.set('selfDocWrite1','자기소개서작성 1회');
-  map.set('selfDocWrite50','자기소개서작성 50회');
-  map.set('selfDocWrite100','자기소개서작성 100회');
-  map.set('jobRead1','채용공고 열람 1회');
-  map.set('jobRead50','채용공고 열람 50회');
-  map.set('jobRead100','채용공고 열람 100회');
-  map.set('jobScrap1','채용공고 스크랩 1회');
-  map.set('jobScrap50','채용공고 스크랩 50회');
-  map.set('jobScrap100','채용공고 스크랩 100회');
+                              "blogScrap1","blogScrap50","blogScrap100","selfDocWrite1","selfDocWrite50","selfDocWrite100",
+                              "game10","game30","game50","game150","game300","jobRead100","jobScrap50","jobScrap100"];  //얻지 못한 배지 정보
+  const map = new Map<String,String>(); 
+  map.set('attend1','출석 1일');
+  map.set('attend50','출석 50일');
+  map.set('attend100','출석 100일');
+  map.set('blogRead1','Blog 열람 1회');
+  map.set('blogRead50','Blog 열람 50회');
+  map.set('blogRead100','Blog 열람 100회');
+  map.set('blogScrap1','Blog 스크랩 1회');
+  map.set('blogScrap50','Blog 스크랩 50회');
+  map.set('blogScrap100','Blog 스크랩 100회');
+  map.set('selfDocWrite1','자소서 작성 1회');
+  map.set('selfDocWrite50','자소서 작성 50회');
+  map.set('selfDocWrite100','자소서 작성 100회');
+  map.set('jobRead1','공고 열람 1회');
+  map.set('jobRead50','공고 열람 50회');
+  map.set('jobRead100','공고 열람 100회');
+  map.set('jobScrap1','공고 스크랩 1회');
+  map.set('jobScrap50','공고 스크랩 50회');
+  map.set('jobScrap100','공고 스크랩 100회');
   map.set('game10',' 게임 승리 10회');
   map.set('game30',' 게임 승리 30회');
   map.set('game50',' 게임 승리 50회');
   map.set('game150',' 게임 승리 150회');
   map.set('game300',' 게임 승리 300회');
-  const [acq, setAcq] = useState<string[]>([]);
-  const [unacq, setUnacq] = useState([]);
+
+  const [acq,setAcq] = useState<ReactElement[]>([]); //react 문 보낼것
+  const [unacq,setUnacq] = useState<ReactElement[]>([]); //react 문 보낼것
+
   useEffect(() => {
     const newElements:ReactElement[] = [];
     for (let i = 0; i < acquired.length; i++) {
       newElements.push(
-        <div key={i} className="h-[10vh] w-[10vh] bg-[#cbffc2] rounded-lg mx-11 my-8">
-          <Image src={`/images/${acquired[i]}.png`} width={100} height={100} alt="badge"/>
+        <div key={i} className="flex flex-col items-center justify-center h-[16vh] w-[16vh]">
+          <div className="flex items-center justify-center">
+            <Image src={`/images/badge/${acquired[i]}.png`} width={100} height={100} alt="badge"/>
+          </div>
+          <div className="flex items-center justify-center text-sm font-bold">{map.get(acquired[i])}</div>
         </div>
       );
     }
     setAcq(newElements);
-  }, [acquired]);
+  }, []);
+
+  useEffect(() => {
+    const newElements:ReactElement[] = [];
+    for (let i = 0; i < unacquired.length; i++) {
+      newElements.push(
+        <div key={i} className="flex flex-col items-center justify-center h-[16vh] w-[16vh]">
+          <div className="h-[100px] w-[100px] flex items-center justify-center">
+            {/* <Image src={`/images/badge/${unacquired[i]}.png`} width={100} height={100} alt="badge"/> */}
+            <Image src={`/images/badge/locked.png`} width={50} height={50} alt="badge"/>
+          </div>
+          <div className="flex items-center justify-center text-sm font-bold">{map.get(unacquired[i])}</div>
+        </div>
+      );
+    }
+    setUnacq(newElements);
+  }, []);
 
   return (
     <div>
       <div className="flex items-center justify-start mt-6">
-        <div className="h-[5vh] w-[15vw] bg-[#CBFFC2] flex items-center justify-center rounded-lg">내가 획득한 뱃지</div>
+        <div className="h-[5vh] w-[15vw] bg-[#CBFFC2] flex items-center justify-center rounded-lg font-bold">내가 획득한 뱃지</div>
       </div>
-      <div className="flex flex-wrap w-full">
+      <div className="flex items-center justify-start flex-wrap w-full">
         {acq}
-        {/* <div className="h-[10vh] w-[10vh] bg-[#cbffc2] rounded-lg mx-11 my-8"/>
-        <div className="h-[10vh] w-[10vh] bg-[#cbffc2] rounded-lg mx-11 my-8"/>
-        <div className="h-[10vh] w-[10vh] bg-[#cbffc2] rounded-lg mx-11 my-8"/>
-        <div className="h-[10vh] w-[10vh] bg-[#cbffc2] rounded-lg mx-11 my-8"/>
-        <div className="h-[10vh] w-[10vh] bg-[#cbffc2] rounded-lg mx-11 my-8"/>
-        <div className="h-[10vh] w-[10vh] bg-[#cbffc2] rounded-lg mx-11 my-8"/> */}
+      </div>
+      <div className="flex items-center justify-start mt-6">
+        <div className="h-[5vh] w-[15vw] bg-[#CBFFC2] flex items-center justify-center rounded-lg font-bold">획득 가능한 뱃지</div>
+      </div>
+      <div className="flex items-center justify-start flex-wrap w-full">
+        {unacq}
       </div>
     </div>
   )
