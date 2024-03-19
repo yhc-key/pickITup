f8c50b170f90ba1c0be38ca9c05e2703b0b459d3
송윤재 <1218song@naver.com>
2024-03-19 15:45:06 +0900

    🚑Hotfix :  add dummy data

--- backend/spring-api/src/main/java/com/ssafy/pickitup/InitDB.java
+++ backend/spring-api/src/main/java/com/ssafy/pickitup/InitDB.java
@@ -34,17 +34,18 @@ public class InitDB {
         //auth 정보 저장
 
         MainQuestionCommandRequestDto mainQuestionCommandRequestDto1 = new MainQuestionCommandRequestDto(
-            "메인 질문 기본값1");
+            "지원동기");
         MainQuestionCommandRequestDto mainQuestionCommandRequestDto2 = new MainQuestionCommandRequestDto(
-            "메인 질문 기본값2");
+            "위기 극복 경험");
         SubQuestionCommandRequestDto subQuestionCommandRequestDto1 = new SubQuestionCommandRequestDto(
-            "서브1", "내용1", "삼성");
+            "삼성전자를 지원한 이유와 입사 후 회사에서 이루고 싶은 꿈을 기술하십시오.", "이 회사 너무 잘 나가서요", "삼성");
         SubQuestionCommandRequestDto subQuestionCommandRequestDto2 = new SubQuestionCommandRequestDto(
-            "서브2", "내용2", "SK");
+            "SK 하이닉스의 지원동기와 입사 후 계획", "이천 밥이 그렇게 맛있다던데", "SK");
         SubQuestionCommandRequestDto subQuestionCommandRequestDto3 = new SubQuestionCommandRequestDto(
-            "서브3", "내용3", "네이버");
+            "자기소개와 함께 지원 동기를 설명해 주세요.", "저는 조은우입니다", "네이버");
         SubQuestionCommandRequestDto subQuestionCommandRequestDto4 = new SubQuestionCommandRequestDto(
-            "서브4", "내용4", "카카오");
+            "프로그램 개발, 알고리즘 풀이 등 SW개발 관련 경험 중 가장 어려웠던 경험과 해결방안에 대해 구체적으로 서술하여 주시기 바랍니다. (과제 개요, 어려웠던 점, 해결방법, 결과 포함)",
+            "개발 너무 어려워요..", "삼성");
 
         authCommandService.signup(userSignupDto1);
         authCommandService.signup(userSignupDto2);
