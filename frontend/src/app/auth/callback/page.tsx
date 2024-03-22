85d868dbe6e01b709513bb6f9ff64781032cde1f
조용환 <yhcho0712@gmail.com>
2024-03-22 09:28:05 +0900

    🤝🏻Merge :  Fix sessionStorage error ing

--- frontend/src/app/auth/callback/page.tsx
+++ frontend/src/app/auth/callback/page.tsx
@@ -1,23 +1,22 @@
-"use client"
-import { useSearchParams,useRouter } from "next/navigation";
-import { useEffect } from "react";
+"use client";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useEffect, Suspense } from "react";
 import useAuthStore from "../../../../store/authStore";
-export default function Callback(){
+export default function Callback() {
   const router = useRouter();
-  const login = useAuthStore(state => state.login);
+  const login = useAuthStore((state) => state.login);
   const searchParams = useSearchParams();
   useEffect(() => {
-    
-    const refreshToken = searchParams.get('refresh-token') ?? '';
-    const accessToken = searchParams.get('access-token') ?? '';
-    const tokenType = searchParams.get('token_type') ?? '';
-    const expiresIn = searchParams.get('expires_in') ?? '';
-    sessionStorage.setItem('tokenType',tokenType);
-    sessionStorage.setItem('accessToken',accessToken);
-    sessionStorage.setItem('refreshToken',refreshToken);
-    sessionStorage.setItem('expiresIn',expiresIn);
+    const refreshToken = searchParams.get("refresh-token") ?? "";
+    const accessToken = searchParams.get("access-token") ?? "";
+    const tokenType = searchParams.get("token_type") ?? "";
+    const expiresIn = searchParams.get("expires_in") ?? "";
+    sessionStorage.setItem("tokenType", tokenType);
+    sessionStorage.setItem("accessToken", accessToken);
+    sessionStorage.setItem("refreshToken", refreshToken);
+    sessionStorage.setItem("expiresIn", expiresIn);
     console.log(searchParams.toString);
-    
+
     console.log(tokenType);
     console.log(accessToken);
     console.log(refreshToken);
@@ -37,10 +36,9 @@ export default function Callback(){
     // })
     // .catch(e=>{alert(e)});
 
-
-    router.push('/');
+    router.push("/");
   }, [searchParams, router]);
-  return(
-    <>hi</>
-  )
+  <Suspense>
+    return <>hi</>;
+  </Suspense>;
 }
