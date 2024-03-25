3f10f7a7c1cdde24aed0d551309dd61d3f1513fb
송윤재 <1218song@naver.com>
2024-03-25 10:04:43 +0900

    🚑Hotfix :  fix recruit dto

---
+++ backend/spring-api/src/main/java/com/ssafy/pickitup/domain/user/query/dto/UserMongoQueryResponseDto.java
@@ -0,0 +1,23 @@
+package com.ssafy.pickitup.domain.user.query.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserMongoQueryResponseDto {
+
+    private Integer id;
+
+    private List<String> keywords;
+    private Integer latitude;
+    private Integer longitude;
+
+}
