ba43d215507b40a47805b7175f4d69fc9aa523a8
노세희 <seahee0130@gmail.com>
2024-03-25 12:25:16 +0900

    🚑Hotfix :  remove unused property

--- frontend/components/onBoarding/dots.tsx
+++ frontend/components/onBoarding/dots.tsx
@@ -6,15 +6,15 @@ const Dot = ({ num, scrollIdx }: { num: number; scrollIdx: number }) => {
   );
 };
 
-export default function Dots({ scrollIdx, handleDotClick }: { scrollIdx: number; handleDotClick: (num: number) => void }) {
+export default function Dots({ scrollIdx }: { scrollIdx: number }) {
   return (
     <div className="fixed top-[35%] right-12">
       <div className="flex flex-col justify-around h-44">
-        <Dot num={1} scrollIdx={scrollIdx} handleDotClick={handleDotClick}></Dot>
-        <Dot num={2} scrollIdx={scrollIdx} handleDotClick={handleDotClick}></Dot>
-        <Dot num={3} scrollIdx={scrollIdx} handleDotClick={handleDotClick}></Dot>
-        <Dot num={4} scrollIdx={scrollIdx} handleDotClick={handleDotClick}></Dot>
-        <Dot num={5} scrollIdx={scrollIdx} handleDotClick={handleDotClick}></Dot>
+        <Dot num={1} scrollIdx={scrollIdx}></Dot>
+        <Dot num={2} scrollIdx={scrollIdx}></Dot>
+        <Dot num={3} scrollIdx={scrollIdx}></Dot>
+        <Dot num={4} scrollIdx={scrollIdx}></Dot>
+        <Dot num={5} scrollIdx={scrollIdx}></Dot>
       </div>
     </div>
   );
