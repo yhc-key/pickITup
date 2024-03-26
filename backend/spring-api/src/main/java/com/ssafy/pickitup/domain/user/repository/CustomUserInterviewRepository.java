9598cc3568da1574110e29306a64ebdc3606bbcf
오형택 <oh052679@gmail.com>
2024-03-27 00:03:47 +0900

    ♻️Refactor :  add EntityGraph for fetch join

--- backend/spring-api/src/main/java/com/ssafy/pickitup/domain/user/repository/CustomUserInterviewRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.pickitup.domain.user.repository;
-
-public interface CustomUserInterviewRepository {
-
-  void createOrUpdateUserInterview(Integer userId, Integer interviewId, String answer);
-}
--- backend/spring-api/src/main/java/com/ssafy/pickitup/domain/user/repository/UserInterviewRepository.java
+++ backend/spring-api/src/main/java/com/ssafy/pickitup/domain/user/repository/UserInterviewRepository.java
@@ -3,11 +3,14 @@ package com.ssafy.pickitup.domain.user.repository;
 import com.ssafy.pickitup.domain.user.entity.UserInterview;
 import java.util.Optional;
 import java.util.stream.Stream;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserInterviewRepository extends JpaRepository<UserInterview, Integer> {
 
+  @EntityGraph(attributePaths = {"interview"})
   Stream<UserInterview> findByUserIdOrderByLastModifiedDateDesc(Integer userId);
 
   Optional<UserInterview> findByUserIdAndInterviewId(Integer userId, Integer interviewId);
