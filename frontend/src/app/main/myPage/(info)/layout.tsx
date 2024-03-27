462cf60b53e877d99f7cebb9e77102e84ccdc410
노세희 <seahee0130@gmail.com>
2024-03-27 17:38:19 +0900

    🎨Design :  update mypage nav active effect

--- frontend/src/app/main/myPage/(info)/layout.tsx
+++ frontend/src/app/main/myPage/(info)/layout.tsx
@@ -15,25 +15,25 @@ export default function InfoLayout({
       <div className="border-b-2 border-black flex flex-row items-center mr-4">
         <Link
           href="/main/myPage/myFavoriteRecruit"
-          className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center hover:text-f5green-300 ${isActive("/myPage/myFavoriteRecruit") ? "border-t-4 border-f5green-300" : ""} `}
+          className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center hover:text-f5green-300 ${isActive("/main/myPage/myFavoriteRecruit") ? "border-t-4 border-f5green-300" : ""} `}
         >
           <span>내가 찜한 채용공고</span>
         </Link>
         <Link
           href="/main/myPage/myBadge"
-          className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center hover:text-f5green-300 ${isActive("/myPage/myBadge") ? "border-t-4 border-f5green-300" : ""} `}
+          className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center hover:text-f5green-300 ${isActive("/main/myPage/myBadge") ? "border-t-4 border-f5green-300" : ""} `}
         >
           <span>나의 뱃지</span>
         </Link>
         <Link
           href="/main/myPage/myPastAns"
-          className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center hover:text-f5green-300 ${isActive("/myPage/myPastAns") ? "border-t-4 border-f5green-300" : ""} `}
+          className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center hover:text-f5green-300 ${isActive("/main/myPage/myPastAns") ? "border-t-4 border-f5green-300" : ""} `}
         >
           <span>나의 과거 문제 내역</span>
         </Link>
         <Link
           href="/main/myPage/myEssay"
-          className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center hover:text-f5green-300 ${isActive("/myPage/myEssay") ? "border-t-4 border-f5green-300" : ""} `}
+          className={`w-1/4 text-f5black-300 h-16 flex items-center justify-center hover:text-f5green-300 ${isActive("/main/myPage/myEssay") ? "border-t-4 border-f5green-300" : ""} `}
         >
           <span>자기소개서 관리</span>
         </Link>
