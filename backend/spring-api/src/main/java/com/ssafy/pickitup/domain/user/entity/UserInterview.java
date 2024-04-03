c88cee7a637e7940a1765b6b11bd3923cf4c495b
hyunsoo10 <hyunsoo4885@gmail.com>
2024-04-04 08:59:41 +0900

    ♻️Refactor :  user entity

--- backend/spring-api/src/main/java/com/ssafy/pickitup/domain/user/entity/UserInterview.java
+++ backend/spring-api/src/main/java/com/ssafy/pickitup/domain/user/entity/UserInterview.java
@@ -9,16 +9,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Getter
@@ -28,21 +24,23 @@ import org.springframework.data.annotation.LastModifiedDate;
 @Table(name = "user_interview")
 public class UserInterview extends BaseTimeEntity {
 
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-  @Column @NotNull
-  private String answer;
+    @Column
+    @NotNull
+    private String answer;
 
-  @OneToOne
-  @JoinColumn(name = "interivew_id", referencedColumnName = "id")
-  private Interview interview;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "interview_id", referencedColumnName = "id")
+    private Interview interview;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id", referencedColumnName = "id")
-  private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 
-  public void updateAnswer(String answer) {
-    this.answer = answer;
-  }
+    public void updateAnswer(String answer) {
+        this.answer = answer;
+    }
 }
