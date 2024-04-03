9edd183ed225fde07cd7b947f118998a991ae126
오형택 <oh052679@gmail.com>
2024-04-03 20:11:11 +0900

    ✨Feat :  utilize Spark session creation for each request

--- backend/play-spark/app/utils/SparkUtil.scala
+++ backend/play-spark/app/utils/SparkUtil.scala
@@ -8,12 +8,13 @@ object SparkUtil {
 
   // 이미 생성된 Spark 세션을 반환하거나 새로운 세션을 생성
   def getOrCreateSparkSession(): SparkSession = {
-    SparkSession.builder
+    val spark = SparkSession.builder
       .appName("PlaySpark")
       .master("local[*]")
       .config("spark.mongodb.input.uri", MONGO_URI)
       .config("spark.mongodb.output.uri", MONGO_URI)
       .getOrCreate()
+    spark.newSession()
   }
 
   // 코사인 유사도 계산을 위한 사용자 정의 함수
